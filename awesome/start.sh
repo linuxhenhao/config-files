c7db657175e026f5e414d89da5f39ff1b082f083
huangyu <diwang90@gmail.com>
2017-10-26 16:52:08 +0800

    update orangepi kernel config: add iptables support

---
+++ awesome/start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+check_and_run()
+{
+	if [ $(ps aux|grep -i $1|wc -l) == "1" ];then
+		$1 &
+	fi
+}
+programs="xcompmgr sogou-qimpanel synap volumeicon nutstore"
+
+for i in $programs
+do
+   check_and_run $i
+done
+   
+
