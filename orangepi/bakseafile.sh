00937d5aac7657fda6808c8ba0ef4e33706b1516
huangyu <diwang90@gmail.com>
2018-03-31 10:06:47 +0800

    updated bakseafile.sh

--- orangepi/bakseafile.sh
+++ orangepi/bakseafile.sh
@@ -2,11 +2,10 @@
 
 cmd=/usr/local/bin/btrfs-backup.py
 destdir=/media/raid1      # Mount backup disk here
-dirs=( '/media/u-sda1' )  # Backup these dirs
 snapdir=.snapshots
 sourcedir=/media/u-sda1  # Mount subvolid=0 here
-nbackup=30            # Keep this many backups on $destdir
-nsnap=30           # keep this many source snapshots on $sourcedir/$snapdir
+nbackup=30            # Keep this number of backups on $destdir
+nsnap=30           # keep this number of source snapshots on $sourcedir/$snapdir
 
 args="--num-backups $nbackup --snapshot-folder ${snapdir} --num-snapshots ${nsnap}"
 echo Executing $cmd $args $sourcedir $destdir
