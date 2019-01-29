fdc26d321c8a420009ac4436dad499231129e9b0
Yu Huang <diwang90@gmail.com>
2019-01-29 22:27:29 +0800

    added fish config# including pyenv config

---
+++ fish/config.fish
@@ -0,0 +1,50 @@
+set -g __fish_git_prompt_show_informative_status 1
+set -g __fish_git_prompt_hide_untrackedfiles 1
+
+set -g __fish_git_prompt_color_branch magenta bold
+set -g __fish_git_prompt_showupstream "informative"
+set -g __fish_git_prompt_char_upstream_ahead "↑"
+set -g __fish_git_prompt_char_upstream_behind "↓"
+set -g __fish_git_prompt_char_upstream_prefix ""
+
+set -g __fish_git_prompt_char_stagedstate "●"
+set -g __fish_git_prompt_char_dirtystate "✚"
+set -g __fish_git_prompt_char_untrackedfiles "…"
+set -g __fish_git_prompt_char_conflictedstate "✖"
+set -g __fish_git_prompt_char_cleanstate "✔"
+
+set -g __fish_git_prompt_color_dirtystate blue
+set -g __fish_git_prompt_color_stagedstate yellow
+set -g __fish_git_prompt_color_invalidstate red
+set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
+set -g __fish_git_prompt_color_cleanstate green bold
+
+set -gx PATH '/home/huangyu/.pyenv/shims' $PATH
+set -gx PATH '/home/huangyu/.pyenv/bin' $PATH
+set -gx PYENV_SHELL fish
+source '/home/huangyu/.pyenv/libexec/../completions/pyenv.fish'
+command pyenv rehash 2>/dev/null
+function pyenv
+  set command $argv[1]
+  set -e argv[1]
+
+  switch "$command"
+  case activate deactivate rehash shell
+    source (pyenv "sh-$command" $argv|psub)
+  case '*'
+    command pyenv "$command" $argv
+  end
+end
+
+set -gx PATH '/home/huangyu/.pyenv/plugins/pyenv-virtualenv/shims' $PATH;
+set -gx PYENV_VIRTUALENV_INIT 1;
+function _pyenv_virtualenv_hook --on-event fish_prompt;
+  set -l ret $status
+  if [ -n "$VIRTUAL_ENV" ]
+    pyenv activate --quiet; or pyenv deactivate --quiet; or true
+  else
+    pyenv activate --quiet; or true
+  end
+  return $ret
+end
+
